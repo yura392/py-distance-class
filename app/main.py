2fab7ecdd9b19ccafe85aec79d824b11777080c1
yura <yura392@gmail.com>
2026-05-19 12:25:13 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,45 @@
+from __future__ import annotations
+from functools import total_ordering
+
+@total_ordering
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int | float) -> None:
+        self.km = km
+
+    def __add__(self, other: int | float | Distance) -> Distance:
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        else:
+            return Distance(self.km + other)
+
+    def __sub__(self, other: int | float | Distance) -> Distance:
+        if isinstance(other, Distance):
+            return Distance(self.km - other.km)
+        else:
+            return Distance(self.km - other)
+
+    def __mul__(self, other: int | float) -> Distance:
+        return Distance(self.km * other)
+
+    def __truediv__(self, other: int | float) -> Distance:
+        if other == 0:
+            raise ZeroDivisionError
+        return Distance(round(self.km / other, 2))
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers"
+
+    def __repr__(self) -> str:
+        return f"Distance({self.km!r})"
+
+    def __eq__(self, other: int | float | Distance) -> bool:
+        if isinstance(other, Distance):
+            return self.km == other.km
+        else:
+            return self.km == other
+
+    def __lt__(self, other: int | float | Distance) -> bool:
+        if isinstance(other, Distance):
+            return self.km < other.km
+        else:
+            return self.km < other
